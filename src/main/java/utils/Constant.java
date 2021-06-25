a957da980b93539b05c28fb9bc7757aeba37c2dd
bobo <893739032@qq.com>
2021-06-26 05:09:20 +0800

    工具类实现

---
+++ src/main/java/utils/Constant.java
@@ -0,0 +1,11 @@
+package utils;
+
+/**
+ * @author bobo
+ * @date 2021/6/25
+ */
+
+public class Constant {
+    public final static String WINDOWS = "Windows";
+    public final static String LINUX = "Linux";
+}
